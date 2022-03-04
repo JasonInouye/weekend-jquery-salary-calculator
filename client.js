c49ef45c1af218adbc8ee45adef1c3d596a9f38b
Jason Inouye <jasonjinouye@gmail.com>
2022-03-04 16:22:30 -0600

    Added rows for submit button and added Annual Salary input

--- client.js
+++ client.js
@@ -9,4 +9,19 @@ function readyNow(){
 
 function addEmployee(){
     console.log( `Submit Button` );
+    let fname = $( `#fnameInput` ).val();
+    let lname = $( `#lnameInput` ).val();
+    let empID = $( `#empIdInput` ).val();
+    let title = $( `#titleInput` ).val();
+    let salary = $( `#salaryInput` ).val();
+    console.log( fname, lname, empID, title );
+    $( `#empRow` ).append(`
+    <tr>
+        <td>${fname}</td>
+        <td>${lname}</td>
+        <td>${empID}</td>
+        <td>${title}</td>
+        <td>${salary}</td>
+    </tr>
+    `)
 }
