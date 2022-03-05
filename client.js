f44ff8833c974f21973bb29b232f3e102e8da651
Jason Inouye <jasonjinouye@gmail.com>
2022-03-05 16:10:07 -0600

    added an array to store employees

--- client.js
+++ client.js
@@ -5,26 +5,58 @@ $(readyNow);
 function readyNow(){
     console.log( `JQuery is loaded` );
     $( `.submitBtn`).on( `click`, addEmployee );
-
+    // you need to load an existing DOM object and use the dynamic ID or CLASS in the parent > child tree
+    $( `#empRow`).on( `click`, `.deleteBtn`, deleteEmployee )
     
+}
+
+const employees = [{}]
+
+function deleteEmployee(){
+    let rowName = $(this).closest( `tr` )
+    console.log( `inside deleteEmployee` );
+    console.log( rowName );
+    $(this).closest( `tr` ).remove();
 }
 
 function addEmployee(){
     console.log( `Submit Button` );
+    
+
+    // storing data into an array in order to get sum of salary
+    const employees = [];
+
     let fname = $( `#fnameInput` ).val();
     let lname = $( `#lnameInput` ).val();
     let empID = $( `#empIdInput` ).val();
     let title = $( `#titleInput` ).val();
     let salary = $( `#salaryInput` ).val();
+
     console.log( fname, lname, empID, title, salary );
+
+    // adding fields to an object
+    let newEmployee = {
+        fname: $( `#fnameInput` ).val(),
+        lname: $( `#lnameInput` ).val(),
+        empID: $( `#empIdInput` ).val(),
+        title: $( `#titleInput` ).val(),
+        salary: $( `#salaryInput` ).val()
+    };
+
+    console.log( newEmployee );
+
+    employees.push( newEmployee );
+
+    console.log( `this is the employees array`, employees );
+    
     $( `#empRow` ).append(`
-        <tr id="${empID}">
+        <tr>
             <td>${fname}</td>
             <td>${lname}</td>
             <td>${empID}</td>
             <td>${title}</td>
             <td>${salary}</td>
-            <td><button id="${empID}">Delete</button></td>
+            <td><button class="deleteBtn">Delete</button></td>
         </tr>
     `)
 
