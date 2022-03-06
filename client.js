5fa9644aa097ad502e89bfc41734b9fd1aec255f
Jason Inouye <jasonjinouye@gmail.com>
2022-03-06 15:03:56 -0600

    commented out exploreThis funciton

--- client.js
+++ client.js
@@ -7,44 +7,44 @@ function readyNow(){
     $( `.submitBtn`).on( `click`, addEmployee );
     //$( `.sumSalary`).on( `click`, monthlySalarySum );
     // you need to load an existing DOM object and use the dynamic ID or CLASS in the parent > child tree
-    //$( `#empRow`).on( `click`, `.deleteBtn`, deleteEmployee );
-    $( `#empRow`).on( `click`, `.deleteBtn`, exploreThis );
-
+    $( `#empRow`).on( `click`, `.deleteBtn`, deleteEmployee );
+    //$( `#empRow`).on( `click`, `.deleteBtn`, exploreThis );
     render();
-    
 }
 
 const employees = [];
 
-
 function deleteEmployee(){
     let rowName = $(this).closest( `td` )
-    console.log( `inside deleteEmployee` );
-    console.log( rowName );
+    // console.log( `inside deleteEmployee` );
+    // console.log( rowName );
     $(this).closest( `td` ).remove();
 }
 
-function exploreThis(){
-    let outputThis = $(this);
-    let outputThis2 = $(this).closest( `td`,`.employeeID` );
-    console.log(`what is outputThis`, outputThis );
-    console.log(`what is outputThis2`, outputThis2 );
-}
-
-exploreThis();
+// My attempt to remove the row in the array with the empID from deleted row.
+// empID is being stored in the table class employeeID
+// function exploreThis(){
+//     let outputThis = $(this);
+//     let outputThis2 = $(this).closest( `td`,`.employeeID` );
+//     console.log(`what is outputThis`, outputThis );
+//     console.log(`what is outputThis2`, outputThis2 );
+//     for (let i = 0; i < employees.length; i++) {
+//         let foundYou = employees[i].empID;
+//         console.log( foundYou );
+//     }
+//     if ( foundYou = 65 ) {
+//         console.log( `Remove this EmployeeID`, foundYou );
+//     }
+// }
 
 function addEmployee(){
-    console.log( `Submit Button` );
-    
-
+    // console.log( `Submit Button` );
     let fname = $( `#fnameInput` ).val();
     let lname = $( `#lnameInput` ).val();
     let empID = $( `#empIdInput` ).val();
     let title = $( `#titleInput` ).val();
     let salary = parseFloat($( `#salaryInput` ).val()).toFixed(2);
-
-    console.log( fname, lname, empID, title, salary );
-
+    // console.log( fname, lname, empID, title, salary );
     // adding fields to an object
     let newEmployee = {
         fname: $( `#fnameInput` ).val(),
@@ -54,13 +54,11 @@ function addEmployee(){
         salary: $( `#salaryInput` ).val()
     };
 
-    console.log( newEmployee );
+    // console.log( newEmployee );
     // storing data into an array in order to get sum of salary
     employees.push( newEmployee );
     render();
-
-    console.log( `this is the employees array`, employees );
-    
+    // console.log( `this is the employees array`, employees );
     $( `#empRow` ).append(`
         <tr>
             <td>${fname}</td>
@@ -94,16 +92,13 @@ function addEmployee(){
 
 function render(){
     let monthlySalary = 0;
-
     for (let i = 0; i < employees.length; i++) {     
         monthlySalary += Number(employees[i].salary/12);     
     }
-
     let outputSalary = parseFloat(monthlySalary).toFixed(2);
-
     // console.log( `this is the monthly salary:`, monthlySalary );
     if (monthlySalary > 20000 ) {
-        console.log( `This is 20000` );
+    // console.log( `This is 20000` );
         $( `#montlyTotal`).text(outputSalary).css("color", "red");
     } else {
         $( `#montlyTotal`).text(outputSalary).css("color", "black");
